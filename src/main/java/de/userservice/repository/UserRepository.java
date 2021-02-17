78c73044011249f8849e753a6349bdf12db45021
Ayrat <ayrat1@mail.ru>
2021-02-17 21:39:11 +0300

    20.00

---
+++ src/main/java/de/userservice/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package de.userservice.repository;
+
+import de.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
